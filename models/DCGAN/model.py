a10a70d8503f1b56d4791575b8bed66da48b69b5
vvi97 <vvi97@yandex-team.ru>
2021-01-06 17:39:24 +0200

    First working version: DCGAN on original 300 paintings. Performance is bleak, but at least it works

--- models/DCGAN/model.py
+++ models/DCGAN/model.py
@@ -1,67 +1,72 @@
 import torch
 
+CONV_INIT_NORMAL_PARAMS = (0, 0.02)
+BATCHNORM_INIT_NORMAL_PARAMS = (1, 0.02)
+BATCHNORM_CONSTANT_INIT_VAL = 0
+
 
 def initialize_weights(layer: torch.nn.Module):
+    """ Initialization suggested in the original DCGAN paper """
     layer_name = layer.__class__.__name__
     if 'Conv' in layer_name:
         layer: torch.nn.Conv2d = layer
-        torch.nn.init.normal_(layer.weight.data, 0.0, 0.02)
+        torch.nn.init.normal_(layer.weight.data, *CONV_INIT_NORMAL_PARAMS)
     elif 'BatchNorm' in layer_name:
         layer: torch.nn.BatchNorm2d = layer
-        torch.nn.init.normal_(layer.weight.data, 1.0, 0.02)
-        torch.nn.init.constant_(layer.bias.data, 0)
+        torch.nn.init.normal_(layer.weight.data, *BATCHNORM_INIT_NORMAL_PARAMS)
+        torch.nn.init.constant_(layer.bias.data, BATCHNORM_CONSTANT_INIT_VAL)
 
 
 class Generator(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, latent_space_dim: int, base_features_depth: int):
         super(Generator, self).__init__()
         self._layers = torch.nn.Sequential(
             torch.nn.ConvTranspose2d(
-                in_channels=100,
-                out_channels=512,
+                in_channels=latent_space_dim,
+                out_channels=base_features_depth * 8,
                 kernel_size=4,
                 stride=1,
                 padding=0,
                 bias=False,
             ),
-            torch.nn.BatchNorm2d(512),
+            torch.nn.BatchNorm2d(base_features_depth * 8),
             torch.nn.ReLU(inplace=True),
             # B x 512 x 4 x 4
             torch.nn.ConvTranspose2d(
-                in_channels=512,
-                out_channels=256,
+                in_channels=base_features_depth * 8,
+                out_channels=base_features_depth * 4,
                 kernel_size=4,
                 stride=2,
                 padding=1,
                 bias=False,
             ),
-            torch.nn.BatchNorm2d(512),
+            torch.nn.BatchNorm2d(base_features_depth * 4),
             torch.nn.ReLU(inplace=True),
             # B x 256 x 8 x 8
             torch.nn.ConvTranspose2d(
-                in_channels=256,
-                out_channels=128,
+                in_channels=base_features_depth * 4,
+                out_channels=base_features_depth * 2,
                 kernel_size=4,
                 stride=2,
                 padding=1,
                 bias=False,
             ),
-            torch.nn.BatchNorm2d(128),
+            torch.nn.BatchNorm2d(base_features_depth * 2),
             torch.nn.ReLU(inplace=True),
             # B x 128 x 16 x 16
             torch.nn.ConvTranspose2d(
-                in_channels=128,
-                out_channels=64,
+                in_channels=base_features_depth * 2,
+                out_channels=base_features_depth,
                 kernel_size=4,
                 stride=2,
                 padding=1,
                 bias=False,
             ),
-            torch.nn.BatchNorm2d(64),
+            torch.nn.BatchNorm2d(base_features_depth),
             torch.nn.ReLU(inplace=True),
             # B x 64 x 32 x32
             torch.nn.ConvTranspose2d(
-                in_channels=64,
+                in_channels=base_features_depth,
                 out_channels=3,
                 kernel_size=4,
                 stride=2,
@@ -81,51 +86,55 @@ class Generator(torch.nn.Module):
 
 
 class Discriminator(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, base_features_depth: int):
         super(Discriminator, self).__init__()
         self._layers = torch.nn.Sequential(
             torch.nn.Conv2d(
                 in_channels=3,
-                out_channels=64,
+                out_channels=base_features_depth,
                 kernel_size=4,
                 stride=2,
                 padding=1,
                 bias=False,
             ),
-            torch.nn.BatchNorm2d(64),
+            torch.nn.BatchNorm2d(base_features_depth),
             torch.nn.LeakyReLU(0.2, inplace=True),
+            # B x 64 x 32 x 32
             torch.nn.Conv2d(
-                in_channels=64,
-                out_channels=128,
+                in_channels=base_features_depth,
+                out_channels=base_features_depth * 2,
                 kernel_size=4,
                 stride=2,
                 padding=1,
                 bias=False,
             ),
-            torch.nn.BatchNorm2d(128),
+            torch.nn.BatchNorm2d(base_features_depth * 2),
             torch.nn.LeakyReLU(0.2, inplace=True),
+            # B x 128 x 16 x 16
             torch.nn.Conv2d(
-                in_channels=128,
-                out_channels=256,
+                in_channels=base_features_depth * 2,
+                out_channels=base_features_depth * 4,
                 kernel_size=4,
                 stride=2,
                 padding=1,
                 bias=False,
             ),
-            torch.nn.BatchNorm2d(256),
+            torch.nn.BatchNorm2d(base_features_depth * 4),
             torch.nn.LeakyReLU(0.2, inplace=True),
+            # B x 256 x 8 x 8
             torch.nn.Conv2d(
-                in_channels=256,
-                out_channels=512,
+                in_channels=base_features_depth * 4,
+                out_channels=base_features_depth * 8,
                 kernel_size=4,
                 stride=2,
                 padding=1,
                 bias=False,
             ),
-            torch.nn.BatchNorm2d(512),
+            torch.nn.BatchNorm2d(base_features_depth * 8),
             torch.nn.LeakyReLU(0.2, inplace=True),
+            # B x 512 x 4 x 4
             torch.nn.Conv2d(
-                in_channels=512,
+                in_channels=base_features_depth * 8,
                 out_channels=1,
                 kernel_size=4,
                 stride=1,
@@ -133,6 +142,7 @@ class Discriminator(torch.nn.Module):
                 bias=False,
             ),
             torch.nn.Sigmoid(),
+            # B x 1 x 1 x 1
         )
 
     def forward(self, input_image: torch.Tensor) -> torch.Tensor:
